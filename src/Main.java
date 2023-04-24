45325de63730e4bd52ce723d04dd6fb30157733f
1cgul <1cgul@gmail.com>
2023-04-24 13:29:17 -0400

    added new class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
+class MyDS{
+    int[] solutionRepo = new int[50001];
+    public void insert(int loc){
+        solutionRepo[loc]=10;
+    }
+    public void remove(int loc){
+        solutionRepo[loc]=0;
+    }
+    public boolean search(int loc){
+        return solutionRepo[loc]==10;
+    }
+}
 public class Main {
+
     public static void main(String[] args) {
 
 
